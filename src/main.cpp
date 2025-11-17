76ca744e1aa3038861076629cb07863c4d7048c6
Sunidhi Singh <sunidhi9826@gmail.com>
2025-11-17 08:59:34 +0530

    Implement Add Product feature

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,50 @@ public:
     }
 };
 
-vector<Product> inventory;   // NEW
+vector<Product> inventory;
+
+void addProduct() {
+    int id, quantity;
+    string name;
+    double price;
+
+    cout << "Enter ID: ";
+    cin >> id;
+
+    cout << "Enter Name: ";
+    cin.ignore();
+    getline(cin, name);
+
+    cout << "Enter Quantity: ";
+    cin >> quantity;
+
+    cout << "Enter Price: ";
+    cin >> price;
+
+    Product p(id, name, quantity, price);
+    inventory.push_back(p);
+
+    cout << "Product Added Successfully!\n";
+}
 
 int main() {
-    cout << "=== Inventory Management System ===\n";
-    cout << "1. Add Product\n";
-    cout << "2. View Products\n";
-    cout << "0. Exit\n";
+    int choice;
+
+    while (true) {
+        cout << "\n=== Inventory Management System ===\n";
+        cout << "1. Add Product\n";
+        cout << "2. View Products\n";
+        cout << "0. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 1)
+            addProduct();
+        else if (choice == 0)
+            break;
+        else
+            cout << "Invalid choice!\n";
+    }
 
     return 0;
 }
