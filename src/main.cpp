04fa8d78bdd7706bfa1a7734a301350d3ce52e88
Sunidhi Singh <sunidhi9826@gmail.com>
2025-11-17 08:53:13 +0530

    Set up basic project structure and main menu

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "=== Inventory Management System ===\n";
+    cout << "1. Add Product\n";
+    cout << "2. View Products\n";
+    cout << "0. Exit\n";
+
+    return 0;
+}
